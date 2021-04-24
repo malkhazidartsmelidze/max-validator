3c32afb851c9db3822711bf15e1ff4ce2284f404
ubermanu <manu@paca.happy-dev.fr>
2021-04-24 17:03:51 +0200

    Update alpha methods tests

--- src/methods.test.js
+++ src/methods.test.js
@@ -4,12 +4,12 @@ it('should validate alpha/numeric characters', () => {
   const { alpha, alpha_dash, alpha_numeric } = methods;
 
   expect(alpha('aAbBcCdD')).toBe(true);
-  expect(alpha('Abc123')).toEqual({ value: 'Abc123' });
+  expect(alpha('Abc123')).not.toBe(true);
 
   expect(alpha_dash('aAbBcCdD')).toBe(true);
   expect(alpha_dash('a-A-b-B-c-')).toBe(true);
-  expect(alpha_dash('aBc-1')).toEqual({ value: 'aBc-1' });
+  expect(alpha_dash('aBc-1')).not.toBe(true);
 
   expect(alpha_numeric('aBc1')).toBe(true);
-  expect(alpha_numeric('aBc-1')).toEqual({ value: 'aBc-1' });
+  expect(alpha_numeric('aBc-1')).not.toBe(true);
 });
