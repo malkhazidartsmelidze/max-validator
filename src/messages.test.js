e80d480c40782a72ab6d7ec71581485c290a69d2
ubermanu <manu@paca.happy-dev.fr>
2021-04-24 16:56:31 +0200

    Add messages tests

---
+++ src/messages.test.js
@@ -0,0 +1,18 @@
+import {
+  messages,
+  defaultMessage,
+  setMessages,
+  setDefaultMessage,
+} from './messages';
+
+it('should replace the default error message', () => {
+  setDefaultMessage('Custom error message');
+  expect(defaultMessage).toBe('Custom error message');
+  expect(() => setDefaultMessage(1234)).toThrowError();
+});
+
+it('should merge/replace messages', () => {
+  setMessages({ replace_rule: 'This is a an example' });
+  expect(Object.keys(messages)).toContain('replace_rule');
+  expect(() => setMessages(1234)).toThrowError();
+});
