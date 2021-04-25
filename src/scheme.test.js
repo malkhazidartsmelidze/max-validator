ca1e6c72e0684d4c1c0abb89fa2ae7d9503c6901
ubermanu <manu@paca.happy-dev.fr>
2021-04-25 20:14:37 +0200

    Add scheme parser tests

---
+++ src/scheme.test.js
@@ -0,0 +1,60 @@
+import { parseScheme } from './scheme';
+import { keys } from 'lodash-es';
+
+it('should parse schemes with strings as rules', () => {
+  const scheme = {
+    name: 'required|string|min:4',
+    age: 'numeric|min:0',
+  };
+
+  expect(() => parseScheme(scheme)).not.toThrowError();
+
+  expect(keys(parseScheme(scheme).name)).toEqual(['required', 'string', 'min']);
+  expect(keys(parseScheme(scheme).age)).toEqual(['numeric', 'min']);
+});
+
+it('should parse schemes with an array of rules', () => {
+  const scheme = {
+    name: ['required', 'string', 'min:4', () => true],
+    age: ['numeric', 'min:0'],
+  };
+
+  expect(() => parseScheme(scheme)).not.toThrowError();
+
+  expect(keys(parseScheme(scheme).name)).toEqual([
+    'required',
+    'string',
+    'min',
+    'anonymous_0',
+  ]);
+
+  expect(keys(parseScheme(scheme).age)).toEqual(['numeric', 'min']);
+});
+
+it('should parse schemes with an object of rules', () => {
+  const scheme = {
+    name: {
+      required: true,
+      string: true,
+      min: 4,
+      test(value) {
+        return value === 'test' || { value };
+      },
+    },
+    age: {
+      numeric: true,
+      min: 0,
+    },
+  };
+
+  expect(() => parseScheme(scheme)).not.toThrowError();
+
+  expect(keys(parseScheme(scheme).name)).toEqual([
+    'required',
+    'string',
+    'min',
+    'test',
+  ]);
+
+  expect(keys(parseScheme(scheme).age)).toEqual(['numeric', 'min']);
+});
